d8eefc0bcb54d1cdfaf99a6c2e705b46acf98c37
OMAR ESTEBAN SICCOS ESTRADA <oese322@hotmail.com>
2016-10-25 16:15:26 -0500

    tercero

--- CapaDeDatos/CapaDeUsuario/frmMantenimientoPaciente.Designer.cs
+++ CapaDeDatos/CapaDeUsuario/frmMantenimientoPaciente.Designer.cs
@@ -28,34 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(62, 33);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(100, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmMantenimientoPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
             this.Name = "frmMantenimientoPaciente";
             this.Text = "frmMantenimientoPaciente";
             this.ResumeLayout(false);
@@ -63,8 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
     }
 }

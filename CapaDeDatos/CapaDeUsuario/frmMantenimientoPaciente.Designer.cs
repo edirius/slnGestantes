cf6f7f8e839aa4eac3cf0cd004999a46cfac282c
OMAR ESTEBAN SICCOS ESTRADA <oese322@hotmail.com>
2016-10-25 16:13:13 -0500

    1

--- CapaDeDatos/CapaDeUsuario/frmMantenimientoPaciente.Designer.cs
+++ CapaDeDatos/CapaDeUsuario/frmMantenimientoPaciente.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -39,11 +40,21 @@
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmMantenimientoPaciente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "frmMantenimientoPaciente";
             this.Text = "frmMantenimientoPaciente";
@@ -54,5 +65,6 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
